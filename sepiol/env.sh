c809a9867d1ab8349b87126e8467e27b1239dd2a
Your Name <madeup@gmail.com>
2020-07-15 03:50:48 -0500

    Removed sample environment.

--- sepiol/env.sh
+++
@@ -1,9 +0,0 @@
-sepiol 
---irc_port=6667 \
---server="irc.rizon.net" \
---nickname="samsepi0l_twitch" \
---target="#ZW9wRxhVTlIz2AzM" \
---twitch_user_id="69836134" \
---endpoint="https://api.twitch.tv/kraken/streams/{}" \
---client_id='8bsovbfuxzk6eg1v8ozwvj7josdbgy' \
---secret='019mhh4x89g6ogbdaryb2rrtfibolk'
